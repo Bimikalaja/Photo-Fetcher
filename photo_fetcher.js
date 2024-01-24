82d5763d4174193359e61eb4f4f0a402a98be741
K05T1N <kostinfloqi15@gmail.com>
2024-01-24 09:08:30 +0100

    test3

--- photo_fetcher.js
+++ photo_fetcher.js
@@ -2,58 +2,71 @@ document.addEventListener("DOMContentLoaded", function () {
   const baseURL = "https://picsum.photos/375/375";
   const photoContainer = document.getElementById("photoContainer");
   const fetchButton = document.getElementById("loadMoreBtn");
-  const morephotosButton = document.getElementById("fetchMoreBtn");
+  const morePhotosButton = document.getElementById("fetchMoreBtn");
   const grayscaleSlider = document.getElementById("toggleSwitch");
   let totalPhotos = 0;
 
   function fetchPhotos(count) {
-    for (let i = 0; i < count; i++) {
-      fetch(baseURL)
-        .then((response) => {
-          if (response.ok) {
-            return response.url;
-          } else {
-            throw new Error("Failed to fetch photo");
-          }
-        })
-        .then((photoUrl) => {
-          const img = document.createElement("img");
-          img.src = photoUrl;
-          img.alt = "Random Photo";
-          photoContainer.appendChild(img);
-          totalPhotos++;
-        })
-        .catch((error) => console.error(error));
-    }
+      for (let i = 0; i < count; i++) {
+          fetch(baseURL)
+              .then((response) => {
+                  if (response.ok) {
+                      return response.url;
+                  } else {
+                      throw new Error("Failed to fetch photo");
+                  }
+              })
+              .then((photoUrl) => {
+                  const photoContainerItem = document.createElement("div");
+                  photoContainerItem.classList.add("photo-container-item");
+
+                  const img = document.createElement("img");
+                  img.src = photoUrl;
+                  img.alt = "Random Photo";
+                  photoContainerItem.appendChild(img);
+
+                  const authorBox = document.createElement("div");
+                  authorBox.classList.add("author");
+                  authorBox.innerHTML = `
+                      <p class="author-name">Lukas Budimaier</p>
+                      <p class="img-link">https://unsplash.com/photos/pwaaqfoMibl</p>
+                  `;
+                  photoContainerItem.appendChild(authorBox);
+
+                  photoContainer.appendChild(photoContainerItem);
+
+                  totalPhotos++;
+              })
+              .catch((error) => console.error(error));
+      }
   }
 
   function applyGrayscale() {
-    const images = photoContainer.querySelectorAll("img");
-    images.forEach((img) => {
-      if (grayscaleSlider.checked) {
-        img.classList.add("grayscale");
-      } else {
-        img.classList.remove("grayscale");
-      }
-    });
+      const images = photoContainer.querySelectorAll(".photo-container-item img");
+      images.forEach((img) => {
+          if (grayscaleSlider.checked) {
+              img.classList.add("grayscale");
+          } else {
+              img.classList.remove("grayscale");
+          }
+      });
   }
 
   function fetchMorePhotos() {
-    fetchPhotos(4); // Fetch four more photos
-    applyGrayscale();
+      fetchPhotos(4); // Fetch four more photos
+      applyGrayscale();
   }
 
   function fetchInitialPhotos() {
-    photoContainer.innerHTML = ""; // Clear existing photos
-    totalPhotos = 0; // Reset totalPhotos counter
-    fetchPhotos(4); // Fetch four new photos
-    applyGrayscale();
+      photoContainer.innerHTML = ""; 
+      totalPhotos = 0; 
+      fetchPhotos(4); 
+      applyGrayscale();
   }
 
-  // Call fetchInitialPhotos when the website opens
   fetchInitialPhotos();
 
   fetchButton.addEventListener("click", fetchInitialPhotos);
-  morephotosButton.addEventListener("click", fetchMorePhotos);
+  morePhotosButton.addEventListener("click", fetchMorePhotos);
   grayscaleSlider.addEventListener("change", applyGrayscale);
 });
